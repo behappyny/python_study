41a6c3c8fc5d3afaa5f49e3b68e6d2d4af0fe97f
behappyny <37570344+behappyny@users.noreply.github.com>
2018-04-12 22:13:48 +0900

    I study python : multiple types !

---
+++ python/20180412 challenges/dict_param.py
@@ -0,0 +1,3 @@
+def foo(**kwargs):
+    print(kwargs)
+foo(bar='bar', hoge='hoge', num=999, name='길벗')
---
+++ python/20180412 challenges/keyword.py
@@ -0,0 +1,6 @@
+def book_info(title, price, publisher):
+    print('도서명 : ', title)
+    print('가격 : ', price)
+    print('출판사 : ', publisher)
+
+book_info(title= '코딩부트캠프', publisher= '길벗', price = 20000)
